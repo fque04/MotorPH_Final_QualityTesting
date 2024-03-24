91fa0bd97bc697b7fc9433d329db1464a08cb420
fque04 <162254695+fque04@users.noreply.github.com>
2024-03-25 01:39:16 +0800

    Update MotorPH.java

--- src/main/java/com/mycompany/motorph/MotorPH.java
+++ src/main/java/com/mycompany/motorph/MotorPH.java
@@ -2,11 +2,15 @@ package com.mycompany.motorph;
 
 import java.io.IOException;
 
-public class MotorPH 
-{
-    public static void main(String[] args) throws IOException
-    {
+// This class serves as an entry point to the program
+public class MotorPH {
+    // The main method, starting point of the program
+    public static void main(String[] args) throws IOException {
+        // Creating an instance of MotorPH_Contents class
         MotorPH_Contents contents = new MotorPH_Contents();
+        
+        // Calling createOptions method of MotorPH_Contents class
+        // and passing the result of LoginPage method as argument
         contents.createOptions(contents.LoginPage());
     }
 }
