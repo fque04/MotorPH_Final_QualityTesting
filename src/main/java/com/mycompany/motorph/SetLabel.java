f993274714ac6a2be5228a5b45e2f407f213cd18
fque04 <162254695+fque04@users.noreply.github.com>
2024-03-25 01:37:37 +0800

    Update SetLabel.java

--- src/main/java/com/mycompany/motorph/SetLabel.java
+++ src/main/java/com/mycompany/motorph/SetLabel.java
@@ -3,11 +3,16 @@ package com.mycompany.motorph;
 import java.io.IOException;
 
 // FORCING A CLASS TO USE THE METHODS INSIDE THE SET LABEL
-public interface SetLabel 
-{
+public interface SetLabel {
+    // Method to set design with options to add header and footer space
     public void setDesign(boolean addHeaderSpace, boolean addFooterSpace);
-    public void createOptions(boolean login) throws IOException;
-    final int maxDesign = 150;
-    public boolean LoginPage();
     
+    // Method to create options
+    public void createOptions(boolean login) throws IOException;
+    
+    // Constant defining maximum design value
+    final int maxDesign = 150;
+    
+    // Method related to login page, returns true if successful
+    public boolean LoginPage();
 }
